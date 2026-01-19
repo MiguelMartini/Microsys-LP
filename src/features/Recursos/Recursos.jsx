5aed240a411ee69068364c9afcf794577824b8f4
MiguelMartini <miguelkaulingm21@gmail.com>
2026-01-19 10:37:39 -0300

    Feat: Recursos do sistema

---
+++ src/features/Recursos/Recursos.jsx
@@ -0,0 +1,24 @@
+import RecursosCard from '@/components/RecursosCard'
+import React from 'react'
+import { Users, BrainCircuit, ChartColumnDecreasing, ShieldCheck } from 'lucide-react';
+
+function Recursos() {
+  return (
+    <div className='p-4 lg:px-40'>
+        <div className='flex items-center flex-col'>
+         <p className='text-2xl font-bold text-gray-600'>Recursos</p>
+         <p className='font-normal'>Recursos poderosos para o seu negócio</p>
+         <div className='mt-4
+         lg:flex'
+         >
+            <RecursosCard icon={<Users className="text-gray-600 w-8 h-8"/>} title={"Gestão Completa"} desc={"Cadastro de empresa, produtos e controle de vendas em um só lugar"}/>
+            <RecursosCard icon={<BrainCircuit className="text-gray-600 w-8 h-8"/>} title={"Assistente IA"} desc={"Inteligência artificial para otimizar decisões e maximizar lucros"}/>
+            <RecursosCard icon={<ChartColumnDecreasing className="text-gray-600 w-8 h-8"/>} title={"Análise de Dados"} desc={"Relatórios inteligentes para acompanhar o crescimento do seu negócio"}/>
+            <RecursosCard icon={<ShieldCheck className="text-gray-600 w-8 h-8"/>} title={"Segurança Total"} desc={"Seus dados protegidos com criptografia de nível empresarial"}/>
+         </div>
+        </div>
+    </div>
+  )
+}
+
+export default Recursos
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../features/Header/Header'
 import MainSection from '../features/MainSection/MainSection'
+import Recursos from '@/features/Recursos/Recursos'
 
 function Home() {
   return (
     <div >
         <Header/>
         <MainSection/>
+        <Recursos/>
     </div>
   )
 }
