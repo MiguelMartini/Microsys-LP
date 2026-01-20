c8f597018153759208d6b29772099fb9499a80f0
MiguelMartini <miguelkaulingm21@gmail.com>
2026-01-20 12:16:52 -0300

    Feat: Tamanhos fixos de buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,10 +3,17 @@ import React from 'react'
 const variants  = { 
     primary: 'bg-blue-600 text-white',
     secondary: 'bg-[#E4EBFF] text-gray-800',
+    transparent: 'bg-transparent text-blue-600 border-blue-600 border-2',
     danger: 'bg-red-600 text-white',
 }
 
-function Button({ title, variant = 'primary', className = '', ...props }) {
+const sizes = {
+  sm: 'w-40 h-10 text-sm',
+  md: 'w-56 h-12 text-base',
+  lg: 'w-72 h-14 text-lg',
+}
+
+function Button({ title, variant = 'primary', className = '', size='sm', ...props }) {
 
   return (
     <div className=''>
@@ -21,6 +28,7 @@ function Button({ title, variant = 'primary', className = '', ...props }) {
         hover:opacity-85
         cursor-pointer
         ${variants[variant]}
+        ${sizes[size]}
         ${className}
       `}
     >
