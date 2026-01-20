9c63c4921e2ecc2ad376f1d5338022ed9360bfd6
MiguelMartini <miguelkaulingm21@gmail.com>
2026-01-20 12:18:15 -0300

    Feat: Buttons na main Section

--- src/features/MainSection/MainSection.jsx
+++ src/features/MainSection/MainSection.jsx
@@ -1,14 +1,20 @@
+import Button from '@/components/Button'
 import React from 'react'
+import { toast } from "sonner"
 
 function MainSection() {
   return (
-    <div className='bg-[#E4EBFF] px-5 py-10'>
+    <div className='bg-[#E4EBFF] px-5 py-15 lg:py-30'>
       <div className='flex flex-col items-center'>
-        <p className='text-2xl font-bold text-gray-700'>Bem-vindo ao <spam className='text-[#2563EB] '>MicroSys</spam></p>
-        <p className='text-lg text-gray-600'>A gestão do seu negócio, simplificada</p>
+        <p className='text-center text-4xl font-bold text-gray-800 lg:text-6xl'>Bem-vindo ao <span className='text-[#2563EB] '>MicroSys</span></p>
+        <p className='text-lg font-medium text-gray-700 mt-2 lg:text-xl'>A gestão do seu negócio, simplificada</p>
       </div>
       <div className='my-5 flex justify-center'>
         <p className='text-gray-600 font-normal text-justify indent-4'>O MicroSys é um sistema para apoio de pequenos e médios empreendedores, com auxílio de agentes de Inteligência Artificial para tomadas de decisões</p>
+      </div>
+      <div className='flex flex-col gap-4 mt-6 items-center md:flex-row md:justify-center'>
+        <Button title={"Começar Grátis"} variant='transparent' size='lg' onClick={() => toast.warning("Entre em Contato")}/>
+        <Button title={"Ver Demonstração"} size='lg' onClick={() => toast.warning("Em construção")}/>
       </div>
     </div>
   )
