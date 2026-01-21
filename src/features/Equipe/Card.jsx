83d5b3971d6d082c6596a62aa706f584a5c96991
MiguelMartini <miguelkaulingm21@gmail.com>
2026-01-21 13:14:49 -0300

    Feat: Equipe MicroSys

--- src/features/Equipe/Card.jsx
+++ src/features/Equipe/Card.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Card({picture ,name, position, about}) {
   return (
-    <div className='flex flex-col items-center bg-gray-100 rounded-2xl p-5 mt-5 w-80'>
+    <div className='flex flex-col items-center bg-gray-100 rounded-2xl p-5 mt-5 w-80 h-90 shadow-xl/20'>
         <div className='bg-gray-400 rounded-full overflow-hidden'>
-            <img src={picture} alt={`foto de perfilde ${name}`} className='w-32 h-32'/>
+            <img src={picture} alt={`foto de perfilde ${name}`} className='w-32 h-32 object-cover'/>
         </div>
         <div className='flex flex-col items-center gap-2 mt-3'>
             <p className='text-xl font-semibold text-gray-700'>{name}</p>
--- src/features/Equipe/Equipe.jsx
+++ src/features/Equipe/Equipe.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Card from './Card'
 import miguel from "../../assets/miguel.jpg"
+import lais from "../../assets/lais.jpg"
+import vinicius from "../../assets/vinicius.jpg"
+
 
 function Equipe() {
   return (
@@ -10,7 +13,9 @@ function Equipe() {
           <p>Profissionais que fazem o MicroSys ser uma realidade</p>
         </div>
         <div className='flex flex-col items-center md:flex md:flex-row md:justify-center md:gap-8'>
-            <Card picture={miguel} name={"Miguel"} position={"CEO & Desenvolvedor"} about={"Analista e Desenvolvedor de Sistemas pelo Centro Universitário SENAC. Desenvolvedor Full-Stack do MicroSys"} />
+            <Card picture={miguel} name={"Miguel Martini"} position={"CTO & Desenvolvedor"} about={"Analista e Desenvolvedor de Sistemas pelo Centro Universitário SENAC. Fundador, Desenvolvedor Full-Stack e Q.A do MicroSys"} />
+            <Card picture={vinicius} name={"Vinicius Gonçalves"} position={"CEO & Desenvolvedor"} about={"Graduando em Ciência da Computação pelo IFSC. Desenvolvedor e analista Full-Stack do MicroSys"} />
+            <Card picture={lais} name={"Lais Varela"} position={"Fundadora & Desenvolvedora"} about={"Graduando em Ciência da Computação pelo IFSC. Desenvolvedora Full-Stack do MicroSys"} />
         </div>
     </div>
   )
